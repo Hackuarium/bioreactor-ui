9f5aece6057d19dd45383b4dd24e8d713b850061
NadjibSb <nadjibsouab@gmail.com>
2021-07-26 18:40:15 +0100

    feat: add device card info

---
+++ src/pages/BroadcastDeviceDetails/DeviceCardInfo.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {
+  BadgeSize,
+  Badge,
+  Button,
+  SvgSolidPencilAlt,
+  SvgSolidArrowCircleLeft,
+} from '../../components/tailwind-ui';
+
+const DeviceCardInfo = ({ device, history }) => {
+  console.log(device);
+  return (
+    <div className="my-2 p-2 flex flex-row items-start rounded-md bg-white shadow ">
+      <SvgSolidArrowCircleLeft
+        className="my-4 mr-2 h-6 w-6 text-neutral-300 cursor-pointer"
+        onClick={() => history.goBack()}
+      />
+      <div className="w-full my-2 flex flex-col">
+        <div className="flex flex-row items-center">
+          <h1 className="mr-4 text-lg font-semibold truncate text-primary-800">
+            {device?.name}
+          </h1>
+          <Badge
+            dot
+            rounded
+            label={device?.connected ? 'Active' : 'Inactive'}
+            size={BadgeSize.SMALL}
+            color={device?.connected ? 'success' : 'neutral'}
+            className="w-min h-min"
+          />
+        </div>
+        <div className="flex flex-row flex-wrap justify-between items-end">
+          <div className="flex flex-col">
+            <h3 className="mt-2 text-xs font-italic text-neutral-600 font-base truncate">
+              <span className="font-semibold">Kind: </span>
+              {`${device?.kind}`}
+            </h3>
+            <h3 className="mt-2 text-xs font-italic text-neutral-600 font-base truncate">
+              <span className="font-semibold">Topic: </span>
+              {`${device?.topic}`}
+            </h3>
+            <h3 className="mt-2 text-xs font-italic text-neutral-600 font-base truncate">
+              <span className="font-semibold">URL: </span>
+              {`${device?.protocol}://${device?.url}:${device?.port}`}
+            </h3>
+          </div>
+          <div className="flex flex-row mt-2 ">
+            <Button
+              size="small"
+              variant="white"
+              className="mr-2 "
+              //   onClick={() => setIsModalOpen(true)}
+            >
+              <SvgSolidPencilAlt className="text-gray-700" />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeviceCardInfo;
--- src/pages/BroadcastDeviceDetails/index.jsx
+++ src/pages/BroadcastDeviceDetails/index.jsx
@@ -4,6 +4,7 @@ import { HorizontalNavigation } from '../../components/tailwind-ui';
 import { connectDevice } from '../../services/broadCastDeviceService';
 import { getDevice } from '../../services/devicesService';
 import Details from './Details';
+import DeviceCardInfo from './DeviceCardInfo';
 
 const DeviceDetails = ({ match }) => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -70,122 +71,8 @@ const DeviceDetails = ({ match }) => {
   }, [currentPage]);
 
   return (
-    <div
-      className="m-4 p-2 shadow-lg "
-      style={{ backgroundColor: 'white', borderRadius: '10px' }}
-    >
-
-    <div class=" text-white flex items-center absolute rounded-full py-4 px-4 shadow-xl bg-pink-500 left-4 -top-6">
-              
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z" />
-                </svg>
-            </div>
-
-    <div class="flex space-x-4 ..." >
-    <div class="flex flex-1 flex-col p-4 bg-gray-200 shadow-md hover:shodow-lg rounded-2xl">
-        <div class="flex items-center justify-between">
-          <div class="flex justify-items-stretch  items-center">
-            
-            <div className=' flex-1  flex-col w-1/2'>
-              <div class="  flex flex-col ml-3 mb-3">
-              
-                <div class=" flex flex-row font-medium leading-none text-gray-900">
-                  
-                  <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="white"  viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
-                  </svg>
-                  Device Name
-                  </div>
-                <p class="ml-8 text-sm text-gray-700 leading-none mt-1">{deviceInfos ? deviceInfos.name : ''}
-                </p>
-              </div>
-              <div class=" flex flex-col ml-3 mb-3">
-                <div class="flex flex-row font-medium leading-none text-gray-900">
-                <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="white" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
-                </svg>
-                  Url
-                </div>
-                <p class="ml-8 text-sm text-gray-700 leading-none mt-1">{deviceInfos ? deviceInfos.url : ''}
-                </p>
-              </div>
-              <div class="flex flex-col ml-3 mb-3">
-                <div class="flex flex-row font-medium leading-none text-gray-900">
-                <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="white" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
-                </svg>
-                  Device kind
-                </div>
-                <p class="ml-8 text-sm text-gray-700 leading-none mt-1">{deviceInfos ? deviceInfos.kind : ''}
-                </p>
-              </div>
-              <div class="flex flex-col ml-3 mb-3">
-                <div class="flex flex-row font-medium leading-none text-gray-900">
-                <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="white" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
-                </svg>
-                  Topic
-                </div>
-                <p class="ml-8 text-sm text-gray-700 leading-none mt-1">{deviceInfos ? deviceInfos.topic : ''}
-                </p>
-              </div>
-            </div>
-            
-          </div>
-        </div>
-      </div>
-
-      <div class="flex flex-1 flex-col p-4 bg-gray-200 shadow-md hover:shodow-lg rounded-2xl">
-        <div class="flex items-center justify-between">
-          <div class="flex justify-items-stretch  items-center">
-             <div className=' flex-1  flex-col w-1/2' style={{width:'50%'}}>
-              <div class="  flex flex-col ml-3 mb-3">
-                <div class="flex flex-row font-medium leading-none text-gray-900">
-                <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="white" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.636 18.364a9 9 0 010-12.728m12.728 0a9 9 0 010 12.728m-9.9-2.829a5 5 0 010-7.07m7.072 0a5 5 0 010 7.07M13 12a1 1 0 11-2 0 1 1 0 012 0z" />
-                </svg>
-                  Status
-                </div>
-                <p class="ml-8 text-sm text-gray-700 leading-none mt-1">Connected
-                </p>
-              </div>
-              <div class=" flex flex-col ml-3 mb-3">
-                <div class=" flex flex-row font-medium leading-none text-gray-900">
-                <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="white" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                </svg>
-                  First Date Entry
-                </div>
-                <p class="ml-8 text-sm text-gray-700 leading-none mt-1">12/10/2021
-                </p>
-              </div>
-              <div class="flex flex-col ml-3 mb-3">
-                <div class="flex flex-row font-medium leading-none text-gray-900">
-                <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="white" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                </svg>
-                  Last Date Entry
-                </div>
-                <p class="ml-8 text-sm text-gray-700 leading-none mt-1">12/10/2021
-                </p>
-              </div>
-              <div class="flex flex-col ml-3 mb-3">
-                <div class="flex flex-row font-medium leading-none text-gray-900">
-                <svg xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" class="mr-2 h-6 w-6" fill="white" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 13l-7 7-7-7m14-8l-7 7-7-7" />
-                </svg>
-                  Number of Entries
-                </div>
-                <p class="ml-8 text-sm text-gray-700 leading-none mt-1">10
-                </p>
-              </div>
-            </div>
-           
-          </div>
-        </div>
-      </div>
-      </div>
+    <div className="mx-4 pb-4">
+      <DeviceCardInfo device={deviceInfos} />
 
       <HorizontalNavigation
         onSelect={(option) => {
@@ -199,16 +86,17 @@ const DeviceDetails = ({ match }) => {
         }))}
       />
 
-     
-      <Details
-        allData={previousData}
-        currentPage={currentPage}
-        count={count}
-        data={data}
-        previousData={previousData}
-        DetailType={selected.value}
-        setCurrentPage={(p) => setCurrentPage(p)}
-      />
+      <div className="p-3 mt-4 sm:m-0 flex flex-col items-center rounded-md sm:rounded-t-none bg-white shadow ">
+        <Details
+          allData={previousData}
+          currentPage={currentPage}
+          count={count}
+          data={data}
+          previousData={previousData}
+          DetailType={selected.value}
+          setCurrentPage={(p) => setCurrentPage(p)}
+        />
+      </div>
     </div>
   );
 };
