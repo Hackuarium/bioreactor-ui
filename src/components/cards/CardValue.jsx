3a513ec978e11cd318b1481feb8cfc48171c3130
NadjibSb <nadjibsouab@gmail.com>
2021-08-03 18:01:52 +0100

    feat: CardValue change color on value changed

--- src/components/cards/CardValue.jsx
+++ src/components/cards/CardValue.jsx
@@ -1,6 +1,10 @@
+import React, { useState } from 'react';
 import clsx from 'clsx';
 
 import { ReactComponent as InfoIcon } from '../../assets/icons/information.svg';
+import { useEffect } from 'react';
+
+const COLOR_CHANGED_TIMEOUT = 700;
 
 /**
  *
@@ -12,6 +16,32 @@ import { ReactComponent as InfoIcon } from '../../assets/icons/information.svg';
  */
 
 const CardValue = ({ title, value, unit, info, className }) => {
+  const [_value, setValue] = useState('');
+  const [color, setColor] = useState(0);
+
+  // change color when value is changed
+  useEffect(() => {
+    let timeout;
+    try {
+      // don't execute it on the first render
+      if (_value) {
+        setValue((_oldV) => {
+          const newV = Number(value);
+          const oldV = Number(_oldV);
+          newV > oldV ? setColor(1) : newV < oldV ? setColor(-1) : setColor(0);
+          timeout = setTimeout(() => {
+            setColor(0);
+          }, COLOR_CHANGED_TIMEOUT);
+        });
+      }
+    } catch (e) {
+      // console.log(e);
+    }
+    setValue(value);
+    return () => timeout && clearTimeout(timeout);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value]);
+
   return (
     <div className={clsx('flex', className)}>
       <div className="w-full m-2 p-2 flex flex-col justify-between items-center sm:items-start rounded-md bg-blue-gray-100 shadow-md">
@@ -34,7 +64,15 @@ const CardValue = ({ title, value, unit, info, className }) => {
           )}
         </div>
         <div className="w-full mt-2 flex flex-row justify-center sm:justify-end items-center">
-          <p className="text-xl font-bold text-black">{value}</p>
+          <p
+            className={clsx(
+              'text-xl font-bold text-neutral-800',
+              color > 0 && 'text-success-600',
+              color < 0 && 'text-danger-600',
+            )}
+          >
+            {_value}
+          </p>
           <p className="ml-1 text-sm font-medium text-gray-500">{unit}</p>
         </div>
       </div>
