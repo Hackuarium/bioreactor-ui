7c252ee5a1682b0a3a6be23a1e9db1be39c41c20
andrescvargasr <andresc.vargasr@gmail.com>
2023-01-16 17:06:49 -0500

    refactor: Move plot from Bioreactor to Plot tab

--- src/pages/LocalDeviceDetails/BioreactorTab.jsx
+++ src/pages/LocalDeviceDetails/BioreactorTab.jsx
@@ -67,22 +67,6 @@ const BioreactorTab = ({ data }) => {
             <CardSteps />
           </div>
       </div>
-      {/* Display Plot */}
-      <div className="flex justify-center ">
-        <Plot
-          data={[
-            {
-              x: [1, 2, 3],
-              y: [2, 6, 3],
-              type: 'scatter',
-              mode: 'lines+markers',
-              marker: {color: 'red'},
-            },
-            {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-          ]}
-          layout={ { title: 'A Fancy Plot'} }
-        />
-      </div>
     </div>
   );
 };
