e03cc9fe4e8800aa97f717e0145228f18ebf31b7
andrescvargasr <andresc.vargasr@gmail.com>
2023-01-18 11:40:09 -0500

    refactor: update form in Bioreactor>Settings tab

--- src/pages/LocalDeviceDetails/BioreactorSettings.jsx
+++ src/pages/LocalDeviceDetails/BioreactorSettings.jsx
@@ -1,14 +1,37 @@
 import { useState, useEffect } from 'react';
 
-import { CardStatus, CardErrors, CardSteps } from '../../components';
+import { DividerCustom } from '../../components';
+
 import { msToTime } from '../../services/util';
 
-const COLOR_CHANGED_TIMEOUT = 700;
-
 const BioreactorSettings = ({ data }) => {
+  const [steps, setSteps] = useState({});
+  const stepsValue = [...Array(16).keys()].map((v) => v+1);
+
+  const handleChange = (event) => {
+    const step = event.target.name;
+    const stepValue = event.target.value;
+    setSteps(values => ({...values, [step]: stepValue}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('steps: ', steps);
+  };
+
+  const stepsForm = stepsValue.map((step) => (
+    <label>Enter Step {step}:
+      <input
+        type="text"
+        name={step}
+        value={steps[`${step}`] || ''}
+        onChange={handleChange}
+      />
+    </label>
+  ));
 
   return (
-    <div className="flex">
+    <div className="flex flex-col">
       {/* Display Awake time */}
       <div className="ml-auto">
         {data?.epoch && (
@@ -19,6 +42,13 @@ const BioreactorSettings = ({ data }) => {
             </span>
           </p>
         )}
+      </div>
+      <DividerCustom title="Edit parameters" />
+      <div className="flex flex-col flex-wrap">
+        <form onSubmit={handleSubmit}>
+          {stepsForm}
+          <input type="submit" />
+        </form>
       </div>
     </div>
   );
