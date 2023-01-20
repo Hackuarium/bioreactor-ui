caaa78034a03d70fa179f88fc5aef5d095e08b23
andrescvargasr <andresc.vargasr@gmail.com>
2023-01-19 22:50:42 -0500

    feat: add useForm in BioreactorSettings tab

--- src/pages/LocalDeviceDetails/BioreactorSettings.jsx
+++ src/pages/LocalDeviceDetails/BioreactorSettings.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 
 import { DividerCustom } from '../../components';
 
@@ -8,16 +9,21 @@ const BioreactorSettings = ({ data }) => {
   const [steps, setSteps] = useState({});
   const stepsValue = [...Array(16).keys()].map((v) => v+1);
 
+  // Form
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+
   const handleChange = (event) => {
     const step = event.target.name;
     const stepValue = event.target.value;
     setSteps(values => ({...values, [step]: stepValue}))
   }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('steps: ', steps);
-  };
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   console.log('steps: ', steps);
+  // };
 
   const stepsForm = stepsValue.map((step) => (
     <label>Enter Step {step}:
@@ -45,8 +51,23 @@ const BioreactorSettings = ({ data }) => {
       </div>
       <DividerCustom title="Edit parameters" />
       <div className="flex flex-col flex-wrap">
-        <form onSubmit={handleSubmit}>
+        {/* <form onSubmit={handleSubmit}>
           {stepsForm}
+          <input type="submit" />
+        </form> */}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input type="number" placeholder="Step 1" {...register("Step 1", {required: true, min: 0, maxLength: 80})} />
+          <input type="number" placeholder="Step 2" {...register("Step 2", {required: true, min: 0, maxLength: 100})} />
+          <input type="text" placeholder="Step 3" {...register("Step 3", {required: true, pattern: /^\S+@\S+$/i})} />
+          <input type="text" placeholder="Step 4" {...register("Step 4", {required: true, minLength: 1, maxLength: 12})} />
+          <select {...register("Title", { required: true })}>
+            <option value="Step">Step</option>
+            <option value="Parameter">Parameter</option>
+          </select>
+
+          <input {...register("Set", { required: true })} type="radio" value="Yes" />
+          <input {...register("Set", { required: true })} type="radio" value="No" />
+
           <input type="submit" />
         </form>
       </div>
